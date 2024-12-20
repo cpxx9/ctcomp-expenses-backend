e5efb28d6c45698b2e105b69762650be466665b8
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-12-20 10:57:40 -0500

    feat: database model"

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,5 @@
 const displayHome = async (req, res) => {
-  res.status(200).json({ status: 'success', data: 'index' });
+  res.status(200).json({ status: 'success', data: 'Root of the API' });
 };
 
 module.exports = {
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,15 +1,11 @@
 const { Router } = require('express');
+const passport = require('passport');
 const { displayHome } = require('../controllers/indexController');
 const { usersRouter } = require('./usersRouter');
-const passport = require('passport');
 
 const indexRouter = Router();
 
-indexRouter.get(
-  '/',
-  passport.authenticate('jwt', { session: false }),
-  displayHome
-);
+indexRouter.get('/', displayHome);
 
 indexRouter.use('/users', usersRouter);
 
